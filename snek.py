b374e9ebc5047d064cc984e2f4ca7ead1472061f
Michel Vuijlsteke from Cthulhu <michel@zog.org>
2018-04-13 19:47:23 +0200

    update

--- snek.py
+++ snek.py
@@ -3,33 +3,42 @@ import sys
 import random
 from pygame.locals import *
 
+
 breedte = 32
 hoogte = 32
 lengte = 1
-resolutie = breedte*10, hoogte*10
+grootte = 20
 
-# pygame.display.set_mode(resolutie, pygame.FULLSCREEN) 
+resolutie = breedte*grootte, hoogte*grootte
+
+#pygame.display.set_mode(resolutie, pygame.FULLSCREEN) 
 
 pygame.init()
 
-DISPLAY = pygame.display.set_mode(resolutie, pygame.FULLSCREEN )
+DISPLAY = pygame.display.set_mode(resolutie)
 GROEN = (0, 255, 0)
 ROOD = (255, 0, 0)
-ZWART = (0, 0, 0)
+ZWART = (0,0,0)
 WIT = (255, 255, 255)
+GRIJS = (20, 51, 51)
 eetgeluid = pygame.mixer.Sound('eet.wav')
-
 slang = [[int(breedte/2), int(hoogte/2)]]
 richting = random.randrange(1,4)
+score = 0
 
 def teken():
-    pygame.draw.rect(DISPLAY, GROEN, (0, 0, 10*breedte-1, 10*hoogte-1))
-    pygame.draw.rect(DISPLAY, ZWART, (1, 1, 10*breedte-3, 10*hoogte-3))
-    
+    pygame.draw.rect(DISPLAY, GROEN, (0, 0, grootte*breedte-1, grootte*hoogte-1))
+    pygame.draw.rect(DISPLAY, ZWART, (1, 1, grootte*breedte-3, grootte*hoogte-3))
+
     for s in slang:
-        pygame.draw.rect(DISPLAY, WIT, (s[0]*10, s[1]*10, 9, 9))
+        pygame.draw.rect(DISPLAY, WIT, (s[0]*grootte, s[1]*grootte, grootte, grootte))
     
-    pygame.draw.rect(DISPLAY, ROOD, (fruit[0]*10, fruit[1]*10, 9, 9))
+    pygame.draw.rect(DISPLAY, ROOD, (fruit[0]*grootte, fruit[1]*grootte, grootte, grootte))
+
+    for w in range(breedte-1):
+        pygame.draw.line(DISPLAY, GRIJS, ((w+1)*grootte, 1), ((w+1)*grootte, hoogte*grootte-2), 1)       
+    for h in range(hoogte-1):
+        pygame.draw.line(DISPLAY, GRIJS, (1, (h+1)*grootte), (breedte*grootte-2, (h+1)*grootte), 1)       
 
     pygame.display.update()
 
@@ -71,13 +80,14 @@ while True:
         slang[0][0] = 0
     if slang[0][1] > hoogte-1: 
         slang[0][1] = 0
-    if slang[0][0] < 0: slang[0][0] = 
-        breedte-1
+    if slang[0][0] < 0: 
+        slang[0][0] = breedte-1
 
     if slang[0] == fruit:
         eetgeluid.play()
         fruit = [random.randrange(1, breedte-1), random.randrange(1, hoogte-1)]
         lengte = lengte + 1
+        score = score + 1
 
     for t in range(lengte-1):
         slang.append(list(oude_slang[t]))
