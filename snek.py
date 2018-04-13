c43138608433f86c054dbb264bd675cff62c21c3
Michel Vuijlsteke from Cthulhu <michel@zog.org>
2018-04-13 22:39:11 +0200

    delay > clock, game over

--- snek.py
+++ snek.py
@@ -1,61 +1,74 @@
 import pygame
 import sys
 import random
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 import time
 from pygame.locals import *
 
 
-breedte = 32
-hoogte = 32
-lengte = 1
-grootte = 20
-
-resolutie = breedte*grootte, hoogte*grootte
+BREEDTE = 32
+HOOGTE = 32
+GROOTTE = 20
+RESOLUTIE = BREEDTE*GROOTTE, HOOGTE*GROOTTE
 
 #pygame.display.set_mode(resolutie, pygame.FULLSCREEN) 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setwarnings(False)
+#GPIO.setup(18,GPIO.OUT)
 
 pygame.init()
+pygame.font.init()
 
-DISPLAY = pygame.display.set_mode(resolutie)
+STATUS = pygame.font.SysFont('Arial', GROOTTE)
+GROOT = pygame.font.SysFont('Arial Bold', GROOTTE*3)
+
+DISPLAY = pygame.display.set_mode(RESOLUTIE)
 GROEN = (0, 255, 0)
 ROOD = (255, 0, 0)
 ZWART = (0,0,0)
 WIT = (255, 255, 255)
 GRIJS = (20, 51, 51)
+PAARS = (144, 0, 144)
 BOVEN = 1
 RECHTS = 2
 ONDER = 3
 LINKS = 4
-eetgeluid = pygame.mixer.Sound('eet.wav')
-slang = [[int(breedte/2), int(hoogte/2)]]
+EETGELUID = pygame.mixer.Sound('eet.wav')
+slang = [[int(BREEDTE/2), int(HOOGTE/2)]]
 richting = random.randrange(1,4)
 score = 0
+lengte = 1
+FPS = 5
+KLOK = pygame.time.Clock()
+GAMEOVER = False
 
 def teken():
-    pygame.draw.rect(DISPLAY, GROEN, (0, 0, grootte*breedte-1, grootte*hoogte-1))
-    pygame.draw.rect(DISPLAY, ZWART, (1, 1, grootte*breedte-3, grootte*hoogte-3))
+    pygame.draw.rect(DISPLAY, GROEN, (0, 0, GROOTTE*BREEDTE-1, GROOTTE*HOOGTE-1))
+    pygame.draw.rect(DISPLAY, ZWART, (1, 1, GROOTTE*BREEDTE-3, GROOTTE*HOOGTE-3))
 
     for s in slang:
-        pygame.draw.rect(DISPLAY, WIT, (s[0]*grootte, s[1]*grootte, grootte, grootte))
+        pygame.draw.rect(DISPLAY, WIT, (s[0]*GROOTTE, s[1]*GROOTTE, GROOTTE, GROOTTE))
     
-    pygame.draw.rect(DISPLAY, ROOD, (fruit[0]*grootte, fruit[1]*grootte, grootte, grootte))
+    pygame.draw.rect(DISPLAY, ROOD, (fruit[0]*GROOTTE, fruit[1]*GROOTTE, GROOTTE, GROOTTE))
 
-    for w in range(breedte-1):
-        pygame.draw.line(DISPLAY, GRIJS, ((w+1)*grootte, 1), ((w+1)*grootte, hoogte*grootte-2), 1)       
-    for h in range(hoogte-1):
-        pygame.draw.line(DISPLAY, GRIJS, (1, (h+1)*grootte), (breedte*grootte-2, (h+1)*grootte), 1)       
+    for w in range(BREEDTE-1):
+        pygame.draw.line(DISPLAY, GRIJS, ((w+1)*GROOTTE, 1), ((w+1)*GROOTTE, HOOGTE*GROOTTE-3), 1)       
+    for h in range(HOOGTE-1):
+        pygame.draw.line(DISPLAY, GRIJS, (1, (h+1)*GROOTTE), (BREEDTE*GROOTTE-3, (h+1)*GROOTTE), 1)  
+
+    scoretekst = STATUS.render('Score: '+str(score), False, PAARS)
+    fpstekst = STATUS.render('Snelheid: '+str(FPS), False, PAARS)
+
+    DISPLAY.blit(scoretekst,(GROOTTE,GROOTTE/2))
+    DISPLAY.blit(fpstekst,(GROOTTE,GROOTTE*2))
 
     pygame.display.update()
 
-fruit = [random.randrange(1,breedte-1), random.randrange(1,hoogte-1)]
+fruit = [random.randrange(1,BREEDTE-1), random.randrange(1,HOOGTE-1)]
 
-while True:
-    GPIO.output(18,GPIO.LOW)
+while GAMEOVER == False:
+    #GPIO.output(18,GPIO.LOW)
     for e in pygame.event.get():
         if e.type == KEYDOWN:
             if e.key == K_ESCAPE:
@@ -85,24 +98,41 @@ while True:
         slang[0][0] = slang[0][0]-1
 
     if slang[0][1] < 0: 
-        slang[0][1] = hoogte-1
-    if slang[0][0] > breedte-1: 
+        slang[0][1] = HOOGTE-1
+    if slang[0][0] > BREEDTE-1: 
         slang[0][0] = 0
-    if slang[0][1] > hoogte-1: 
+    if slang[0][1] > HOOGTE-1: 
         slang[0][1] = 0
     if slang[0][0] < 0: 
-        slang[0][0] = breedte-1
+        slang[0][0] = BREEDTE-1
+
+    if slang[0] in oude_slang:
+        GAMEOVER = True
 
     if slang[0] == fruit:
-        eetgeluid.play()
-        fruit = [random.randrange(1, breedte-1), random.randrange(1, hoogte-1)]
+        EETGELUID.play()
+        fruit = [random.randrange(1, BREEDTE-1), random.randrange(1, HOOGTE-1)]
         lengte = lengte + 1
         score = score + 1
-        GPIO.output(18,GPIO.HIGH)
+        #GPIO.output(18,GPIO.HIGH)
+        FPS = min(5 + int(score/5), 25)
 
     for t in range(lengte-1):
         slang.append(list(oude_slang[t]))
 
     teken()
 
-    pygame.time.delay(240-int(lengte/5)*20)
+    KLOK.tick(FPS)
+    
+gameovertekst = GROOT.render('GAME OVER', False, ROOD)
+scoretekst = STATUS.render('Score: '+str(score), False, ROOD)
+DISPLAY.blit(gameovertekst,(BREEDTE*GROOTTE/2-gameovertekst.get_width()/2,HOOGTE*GROOTTE/2-GROOTTE))
+DISPLAY.blit(scoretekst,(BREEDTE*GROOTTE/2-scoretekst.get_width()/2,HOOGTE*GROOTTE/2+GROOTTE))
+pygame.display.update()
+
+while True:
+    for e in pygame.event.get():
+        if e.type == KEYDOWN:
+            if e.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
